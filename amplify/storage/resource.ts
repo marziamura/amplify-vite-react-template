4a9df21ebab4829d0b968ec1b8d3de633a0f10f2
Marzia Mura <marzimur@amazon.ch>
2024-05-31 16:05:54 +0200

    added missing file

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,16 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+
+export const storage = defineStorage({
+    name: 's3testbucketforvideodemo',
+    access: (allow) => ({
+      'profile-pictures/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'picture-submissions/*': [
+        allow.authenticated.to(['read','write']),
+        allow.guest.to(['read', 'write'])
+      ],
+    })
+  });
