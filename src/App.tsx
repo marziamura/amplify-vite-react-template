dc54b161a2e11a2373d3a6e2591d8016816d464e
Marzia Mura <marzimur@amazon.ch>
2024-05-31 15:12:49 +0200

    added storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import React from 'react'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+
 import { Authenticator } from '@aws-amplify/ui-react'
+import { uploadData } from 'aws-amplify/storage';
 import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [file, setFile] = React.useState();
+
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -34,11 +42,19 @@ function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
+        <div>
+      <input type="file" onChange={handleChange} />
+      <button
+          onClick={() =>
+            uploadData({
+              path: `picture-submissions/${file.name}`,
+              data: file,
+          })
+        }
+      >
+        Upload
+      </button>
+      </div>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>
